d7cd3db9d6ca790ac306e9afb0f6828c5045f33a
jeffreyvh <jeffrey25011998@gmail.com>
2015-12-10 16:11:56 +0100

    Update tulip-vendor.mk

--- tulip/tulip-vendor.mk
+++ tulip/tulip-vendor.mk
@@ -1,4 +1,5 @@
 PRODUCT_PLATFORM := kanuti
 $(call inherit-product, vendor/qcom/prebuilt/qcom-vendor.mk)
 $(call inherit-product, vendor/sony/kanuti/kanuti-partial.mk)
+$(call inherit-product, vendor/oppo/msm8939-common/msm8939-common-vendor-blobs.mk)
 $(call inherit-product, $(LOCAL_PATH)/tulip-partial.mk)
