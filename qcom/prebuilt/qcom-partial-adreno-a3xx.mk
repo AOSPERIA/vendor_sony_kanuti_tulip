8065a01b9fc994547549c1679ead53bec4e27f09
jeffreyvh <jeffrey25011998@gmail.com>
2015-12-17 09:31:21 +0100

    Beta Vendor 0.1

---
+++ qcom/prebuilt/qcom-partial-adreno-a3xx.mk
@@ -0,0 +1,31 @@
+# Copyright (C) 2012 Sony Mobile Communication
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+PRODUCT_PACKAGES += \
+	eglsubAndroid \
+	libEGL_adreno \
+	libGLESv1_CM_adreno \
+	libGLESv2_adreno \
+	libq3dtools_adreno \
+	libC2D2 \
+	libCB \
+	libRSDriver_adreno \
+	libadreno_utils \
+	libc2d30-a3xx \
+	libgsl \
+	libllvm-qcom \
+	librs_adreno \
+	librs_adreno_sha1 \
+        libbccQTI \
+	libsc-a3xx
